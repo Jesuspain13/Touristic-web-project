85d3fdc522cc5e305dcd1af7bb71a5a3867e808e
Jesuspain13 <43440788+Jesuspain13@users.noreply.github.com>
2018-12-17 22:21:34 +0100

    pagina web
    mostrar contenido desde js.

--- bootstrap-navbar/codigo.js
+++ bootstrap-navbar/codigo.js
@@ -1,3 +1,15 @@
+/*document.getElementById("tourism-menu").onclick = showSubMenu;
+
+function showSubMenu() {
+	document.getElementById("dropdown-menu").innerHTML = 
+		`<ul>
+			<li>Alcazaba</li>
+			<li>Centro Histórico</li>
+			<li>Muelle 1</li>
+		</ul>`;
+}*/
+
+
 $("#navbarDropdown").click(function(){
 	$("#dropdown-menu-recomended").html( 
 		`
@@ -9,5 +21,27 @@ $("#navbarDropdown").click(function(){
 		</li>
         <li class="dropdown-item">
 		  <a href="#">Lighthouse</a>
-		</li>`)
+		</li>`);
+
+	$("#enlaceCatedral").click(function(){
+		$("#contenedorPrincipal").html(
+			`
+			<div class="row justify-content-center mb-2">
+		      <h2 class="display-3">Catedral de malaga</h2>
+		    </div>
+		    <div class="row justify-content-center ">
+		      <div id="imagenCatedral" class="col-7 float-left border-right border-dark pr-4">
+		        <img class="rounded" src=https://cdn1.guias-viajar.com/wp-content/uploads/2011/12/Catedral-M%C3%A1laga-003-2.jpg" 
+		           width="100%" height="500">
+		      </div>
+		      <div class="col-3 float-left justify-content-left pl-4">
+		        <h4 id="tituloDescripcion" >Pequeña introducción</h4>
+		        <p>info sobre la catedral pojewfoiew jfoeiwjfiowe fiowefowonfwoenfweifnwonffroiwe nfoiwenfoiwenfr   oiwoiwerfo wiowjrw eiofwerfo
+		        </p>
+		      </div>
+		    </div>
+		    `);
+
+		var primeraFoto = "http://sevilleventours.com/wp-content/uploads/2017/05/malaga-visitas-guiadas-sevilleventours.jpg"
+		$(".first-slide").attr("src", primeraFoto);	});
 });
